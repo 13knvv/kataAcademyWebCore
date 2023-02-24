05c1addf6b4b505bb89df5ef48f95e788b444b51
Knyazev Vitaly <13.kn.v.v@gmail.com>
2023-02-24 18:15:29 +0300

    add grid and add js logic btn show more

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { cl } from './js/cl'
 import './styles/styles.scss'
+import { showMore } from './js/show-more'
 
 cl(['it is work'])
+showMore()
---
+++ src/js/show-more.js
@@ -0,0 +1,28 @@
+import {cl} from './cl'
+
+export function showMore() {
+  const btnMore = document.querySelectorAll('.btn-more')
+
+  for (let i = 0; i < btnMore.length; i++) {
+    btnMore[i].addEventListener('click', () => onClickBtnMore(btnMore[i]))
+  }
+
+  function onClickBtnMore(btn) {
+    const content = btn.previousElementSibling
+    const contentHeight = content.style.maxHeight
+    const contentFullHeight = content.scrollHeight + 'px'
+    const smallHeight = content.dataset.smallHeight
+
+    if (contentHeight !== contentFullHeight) {
+      btn.previousElementSibling.style.maxHeight = contentFullHeight
+      btn.textContent = 'Скрыть'
+      btn.classList.add('btn-more--showed')
+    } else {
+      btn.previousElementSibling.style.maxHeight = smallHeight
+      btn.textContent = 'Показать все'
+      btn.classList.remove('btn-more--showed')
+    }
+
+    cl(btn.previousElementSibling.style.maxHeight)
+  }
+}
