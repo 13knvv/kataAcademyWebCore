6e7e717cfc9d5138cc0352eb4a04c3b0263ae5dd
Виталий Князев <vitalijknazev@Air-kn13.netis>
2023-03-02 22:52:36 +0300

    delete div swiper-slide

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+import { initSwiper } from './js/init-swiper'
 import './styles/styles.scss'
 import { btnShowMore } from './js/btn-show-more'
-import { initSwiper } from './js/init-swiper'
 
 
 btnShowMore()
