a5604c69c948a1621e71e11a569229be37069624
Виталий Князев <vitalijknazev@Air-kn13.netis>
2023-03-04 16:07:07 +0300

    change js btn-swow-more

--- src/js/btn-show-more.js
+++ src/js/btn-show-more.js
@@ -7,16 +7,19 @@ export function btnShowMore() {
 
   function onClickBtnMore(btn) {
     const content = btn.previousElementSibling
-    const contentHeight = content.style.maxHeight
-    const contentFullHeight = content.scrollHeight + 'px'
-    const smallHeight = content.dataset.smallHeight
+    content.classList.toggle('drop-down-grid--open')
+    // в drop-down-grid--open задан max-heigth: 1000px; (все что кроме px не работает transition)
+    // поэтому  быстро окрывается и долго закрывается. если задавать настоящую высоту то будет плавно. 
+    //const fullHeight = content.scrollHeight
 
-    if (contentHeight !== contentFullHeight) {
-      btn.previousElementSibling.style.maxHeight = contentFullHeight
+    if (content.classList.contains('drop-down-grid--open')) {
+      // задали бы инлайново высоту но как понял это плохая практика
+      //content.style.maxHeight = fullHeight + 'px'
       btn.textContent = 'Скрыть'
       btn.classList.add('btn-more--showed')
     } else {
-      btn.previousElementSibling.style.maxHeight = smallHeight
+      // удаляем инлайн стили и применется высота изначальная в css
+      //content.removeAttribute('style')
       btn.textContent = 'Показать все'
       btn.classList.remove('btn-more--showed')
     }
