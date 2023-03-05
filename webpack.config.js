552e67a23fee290be7df86e80e0074773d3e8819
Knyazev Vitaly <13.kn.v.v@gmail.com>
2023-03-06 01:57:25 +0300

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,18 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ]
+          }
+        }
+      },
+      {
         test: /\.s[ac]ss$/i,
         use: [
           MiniCssExtractPlugin.loader,
